3c2f655dc88be647c5418da647b9ff98ed045836
satdrasing <satendra.kohli@gmail.com>
2019-12-12 12:21:38 +0530

    application context bootstrap way

--- src/main/java/com/satendra/springsession/SpringSessionApplication.java
+++ src/main/java/com/satendra/springsession/SpringSessionApplication.java
@@ -1,15 +1,21 @@
 package com.satendra.springsession;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
-import org.springframework.web.server.session.WebSessionManager;
 
 @SpringBootApplication
 @EnableJdbcHttpSession
 public class SpringSessionApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringSessionApplication.class, args);
+		//SpringApplication.run(SpringSessionApplication.class, args);
+
+		//another way to bootstrap spring application context
+		SpringApplication springApplication = new SpringApplication(SpringSessionApplication.class);
+		springApplication.setBannerMode(Banner.Mode.OFF);
+		springApplication.run(args);
+
 	}
 }
